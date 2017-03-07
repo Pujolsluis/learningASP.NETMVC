d7a236e4c537c0c271a3ab3019c73783f6caecd1
Luis Pujols <luispujols@hotmail.com>
2017-03-06 23:14:59 -0400

    REFACTOR 'Added the ID parameter to the welcome action and now im able to use the 3rd route to access the action that maps to the ID action'

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -15,9 +15,9 @@ namespace MvcMovie.Controllers
         }
 
         //GET: /HelloWorld/Welcome/
-        public string Welcome(string name, int numTimes = 1)
+        public string Welcome(string name, int ID = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", Numtimes is: " + numTimes);
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
         }
     }
 }
