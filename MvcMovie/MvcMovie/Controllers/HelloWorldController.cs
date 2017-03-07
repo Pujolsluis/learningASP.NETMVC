fa6f8d5a4f43f90d609570b054e2eb78a59f19d9
Luis Pujols <luispujols@hotmail.com>
2017-03-06 23:10:56 -0400

    ADD 'Created the custom parameters for the welcome action'

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -8,10 +8,16 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: HelloWorld
-        public ActionResult Index()
+        // GET: /HelloWorld/
+        public string Index()
         {
-            return View();
+            return "This is my <b>default</b> action...";
+        }
+
+        //GET: /HelloWorld/Welcome/
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", Numtimes is: " + numTimes);
         }
     }
 }
