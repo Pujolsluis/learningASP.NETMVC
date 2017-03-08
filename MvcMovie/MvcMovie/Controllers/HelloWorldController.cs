ed2cf6e00e9d8188d1edd58bdcf2b8787c6b67d9
Luis Pujols <luispujols@hotmail.com>
2017-03-07 23:22:22 -0400

    ADD 'Created the dinamic data with ViewBag  for the welcome layout'

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -15,9 +15,12 @@ namespace MvcMovie.Controllers
         }
 
         //GET: /HelloWorld/Welcome/
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
         }
     }
 }
